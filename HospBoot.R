a493c4da1ed31d78f1b88f999d6b44db09ea0715
Alejandro Salazar <asalazar@colson.edu.mx>
2023-06-20 14:18:15 -0700

    Add files via upload

--- HospBoot.R
+++ HospBoot.R
@@ -13,20 +13,33 @@ supmuni=supmuni%>%select(clave_mun,AREA)
 
 hosp=merge(hosp,supmuni,by="clave_mun")
 
+metros<- read_csv("https://raw.githubusercontent.com/asalazaradams/data/main/ZM_2015.csv")
+metros$clave_mun=metros$CVE_MUN
+metros=metros%>%select(clave_mun,CVE_ZM,NOM_ZM)
+#metros=metros%>%filter(AREAM!="00")
+hosp=merge(hosp,metros, by="clave_mun", all.x =T)
+is.na(hosp$CVE_ZM)
+
+hosp$loc=hosp$CVE_ZM
+
+hosp=hosp %>% 
+  mutate(loc = coalesce(CVE_ZM,clave_mun))
+
+
 hosp$servs=hosp$consulta+hosp$procedq
 
-hosp%>%group_by(muni)%>%
+hosp%>%group_by(loc)%>%
   summarise(tot_servs=sum(servs))
 
-smuni=hosp%>%group_by(muni)%>%
+smuni=hosp%>%group_by(loc)%>%
   summarise(tot_servs=sum(servs))
 
 hosp=merge(hosp,smuni, all=T)
 hosp$mshare=hosp$servs/hosp$tot_servs*100
 hosp$mshare2=hosp$mshare**2
-hh=hosp%>%group_by(muni)%>%
+hh=hosp%>%group_by(loc)%>%
   summarise(hhi=sum(mshare2))
-hosp=merge(hosp,hh,by="muni",all=T)
+hosp=merge(hosp,hh,by="loc",all=T)
 
 hhest=hosp%>%group_by(SREENTIDAD)%>%
   summarise(mean(hhi))
@@ -70,7 +83,6 @@ rsu.env=hosp%>% select( POBTOT,
                         gral,
                         chico,
                         grande,
-                        staff_ac,
                         metro,
                         centro,
                         noroeste,
